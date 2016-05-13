f0ef578a23fa2965f0a8305f060d0b9345672163
seanliu93 <sean93liu@gmail.com>
2016-05-12 23:45:32 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(name) {
+  return "I love " + name;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
